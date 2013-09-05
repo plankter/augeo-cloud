c7166ca42545f156e0db56b0aabce8be3159c824
Anton Rau <anton.rau@gmail.com>
2013-09-05 21:26:15 +0200

    WIP

--- augeo/settings.py
+++ augeo/settings.py
@@ -15,6 +15,7 @@ DATABASES = {
     'default': dj_database_url.config(default='postgresql://localhost/augeo')
 }
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -60,23 +61,24 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Additional locations of static files
 STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'static'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
